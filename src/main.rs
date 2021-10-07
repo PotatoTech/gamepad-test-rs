90e4647bee2741c54504177fee6847724ec05140
PotatoTech <42846367+PotatoTech@users.noreply.github.com>
2021-10-06 21:47:19 -0700

    Add options from C version

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (c) 2021 PotatoTech
 
+use clap::{AppSettings, Clap};
 use sdl2::controller::{Axis, Button, GameController};
 use sdl2::event::Event;
 use sdl2::GameControllerSubsystem;
 use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+
+fn load_mappings<P: AsRef<Path>>(contoller_subsystem: &GameControllerSubsystem, path: P) {
+    match contoller_subsystem.load_mappings(path) {
+        Ok(_) => (),
+        Err(_) => eprintln!("Could not load mappings file"),
+    }
+}
 
 fn handle_gamepad_added(
     contoller_subsystem: &GameControllerSubsystem,
     gamepads: &mut HashMap<u32, GameController>,
     joystick_index: u32,
+    show_mappings: bool,
 ) {
     match contoller_subsystem.open(joystick_index) {
         Ok(gamepad) => {
             let id = gamepad.instance_id();
             gamepads.insert(id, gamepad);
             println!("Added gamepad {}", id);
+            if show_mappings {
+                println!("mapping: {}", gamepads.get(&id).unwrap().mapping());
+            }
         }
         Err(_) => eprintln!("Could not open device {}", joystick_index),
     }
@@ -83,8 +96,14 @@ fn handle_button_up(joystick_id: u32, button: Button) {
 }
 
 fn main() {
+    let opts = Opts::parse();
+
     let sdl_context = sdl2::init().unwrap();
     let contoller_subsystem = sdl_context.game_controller().unwrap();
+
+    if let Some(path) = opts.mappings {
+        load_mappings(&contoller_subsystem, path);
+    }
 
     let mut gamepads = HashMap::<u32, GameController>::new();
 
@@ -93,7 +112,7 @@ fn main() {
         for event in event_pump.poll_iter() {
             match event {
                 Event::ControllerDeviceAdded { which, .. } => {
-                    handle_gamepad_added(&contoller_subsystem, &mut gamepads, which)
+                    handle_gamepad_added(&contoller_subsystem, &mut gamepads, which, opts.debug)
                 }
                 Event::ControllerDeviceRemoved { which, .. } => {
                     handle_gamepad_removed(&mut gamepads, which)
@@ -110,4 +129,15 @@ fn main() {
             }
         }
     }
+}
+
+#[derive(Clap)]
+#[clap(setting = AppSettings::DisableVersionFlag)]
+struct Opts {
+    /// Show the mapping used by each controller when it is added
+    #[clap(short, long)]
+    debug: bool,
+    /// Load the mappings from the specified file
+    #[clap(short, long)]
+    mappings: Option<PathBuf>,
 }
